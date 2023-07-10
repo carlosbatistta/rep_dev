f537cb87663d40a0de6c9b3dc0a6fd87b5dec921
CRLBTT <123827773+CRLBTT@users.noreply.github.com>
2023-07-10 18:04:43 -0300

    excluir arquivo teste

--- portfolio/lib/ux/sections/section_widget.dart
+++
@@ -1,7 +0,0 @@
-abstract class SectionWidget {
-  /// The title for this section.
-  String get title;
-
-  /// Indicates if this section should be selectable in a tab bar.
-  bool get isSelectable => true;
-}
