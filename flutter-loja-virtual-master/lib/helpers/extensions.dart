13258545065ef21a34cdbd57b86f5118e4dae837
CRLBTT <123827773+CRLBTT@users.noreply.github.com>
2023-07-20 18:23:18 -0300

    teste

--- flutter-loja-virtual-master/lib/helpers/extensions.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension Extra on TimeOfDay {
-  String formatted() {
-    return '${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}';
-  }
-
-  int toMinutes() => hour * 60 + minute;
-}
