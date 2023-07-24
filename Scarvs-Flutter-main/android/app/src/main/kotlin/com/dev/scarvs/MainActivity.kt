9d1bff7cfb487b8d7ff904666a3cffd3f0ac9bfc
CRLBTT <123827773+CRLBTT@users.noreply.github.com>
2023-07-24 14:33:09 -0300

    organização de pastas

--- Scarvs-Flutter-main/android/app/src/main/kotlin/com/dev/scarvs/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.dev.scarvs
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Scarvs-Flutter-main/lib/app/routes/api.routes.dart
+++
@@ -1,3 +0,0 @@
-class ApiRoutes {
-  static const String baseurl = "http://10.0.2.2:8080";
-}
