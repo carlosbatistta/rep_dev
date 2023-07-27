2747d885855d4b822d004ba947a35707e8bdbbc1
CRLBTT <123827773+CRLBTT@users.noreply.github.com>
2023-07-27 17:59:32 -0300

    Aula 2

--- dev/ola_mundo/lib/main.dart
+++ dev/ola_mundo/lib/main.dart
@@ -3,15 +3,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // ignore: prefer_const_constructors
-  runApp(
-    MaterialApp(
+  //runApp(MyApp(title: "Barra de teste"));
+  runApp(App());
+}
+
+class MyApp extends StatelessWidget {
+  final String title;
+  //statelesswidget é uma das principais classes do flutter, são locais estáticos da pagina, geralmente Menus, Help, etc
+  const MyApp({Key? key, this.title = ""}) : super(key: key); //contrutor
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       /*O MaterialApp é um widget de conveniência que envolve vários widgets que normalmente são necessários para aplicativos de material design. O widget MaterialApp fornece um layout orientado ao material design.*/
       //Consegue colocar Widgets, dentro de outro widgets.
       home: Scaffold(
-        //scaffold é uma estrutura básica de layout com uma barra superior e informações abaixo.
+        //scaffold é uma estrutura básica de layout com uma barra superior appBar e informações abaixo body
         appBar: AppBar(
-          title: Text('Hello'),
+          title: Text(title),
         ),
         body: Center(
           //body é o corpo do widget scaffold
@@ -21,17 +29,31 @@ void main() {
           ), //é aconselhável separa por vírgulas seus widgets. O widget TEXT possui vários parâmetros, sendo apenas o String data é obrigatória
         ),
       ),
-    ),
-  );
+    );
+  }
 }
 
-class MyApp extends StatelessWidget {
-  //statelesswidget é uma das principais classes do flutter, são locais estáticos da pagina, geralmente Menus, Help, etc
-  const MyApp(
-      {super.key}); //const é um construtor, o super usar o parametro Key da classe pai
+class App extends StatefulWidget {
+  //statefulwidget são widgets dinâmicos que mudam na IA, essa primeira classe serão guardados os apps que não serão mudados
+  final String nome = "Carlos";
+  const App({Key? key}) : super(key: key); // construtor
 
   @override
+  State<App> createState() => _App();
+}
+
+class _App extends State<App> {
+  int salario = 500;
+  //armazenados os dados alteráveis
+  //a chamada vai criar essa classe do tipo state e com um método build, com a função de ataualização da página
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    //widget build irá fazer a contrução da página
+    return Container(
+      child: Center(
+        child: Text("Funcionário: ${widget.nome}",
+            textDirection: TextDirection.ltr),
+      ),
+    );
   }
 }
