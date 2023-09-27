3ddee52f52e34a43c8984a6a3e8a1c55b6c0b5db
CRLBTT <123827773+CRLBTT@users.noreply.github.com>
2023-09-27 06:35:34 -0300

    python hasteg 01

---
+++ dev/python/jornada/automacao.py
@@ -0,0 +1,13 @@
+# site fictício: https://dlp.hashtagtreinamentos.com/python/intensivao/login
+    # realizar login
+    #importar a base de produtos
+    #cadastrar um produto
+    #repetir cadastro
+#instalar biblioteca pyautogui: pip3 install pyautogui
+
+import pyautogui
+
+pyautogui.PAUSE = 0.5
+pyautogui.press("win")
+pyautogui.write("microsoft edge")
+pyautogui.press("enter")
