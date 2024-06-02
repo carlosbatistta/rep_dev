cee5d0ba165f9450186bc35b03c2ee13aea3c0d8
carlosbatistta <123827773+carlosbatistta@users.noreply.github.com>
2024-06-02 20:19:59 -0300

    volta para py

--- dev/python/jornada/quebrando_num_string.py
+++ dev/python/jornada/quebrando_num_string.py
@@ -5,6 +5,7 @@ qualquer pelo teclado e mostre na tela a sua porção Inteira.
 
 import math
 
-numero = float(input('Digite um valor: '))
+numero = input('Digite um valor: ')
+numero_convertido = float(numero)
 ## importada a biblioteca math, o trunc "arredonda" números float para inteiro
-print('o valor digitado foi: {}, sua parte inteira foi:, seu ponto flutuante foi: {}'.format( math.trunc(numero), numero))
+print('o valor digitado foi: {}, sua parte inteira foi:{}, seu ponto flutuante foi: {}'.format(numero, math.trunc(numero_convertido), numero_convertido))
