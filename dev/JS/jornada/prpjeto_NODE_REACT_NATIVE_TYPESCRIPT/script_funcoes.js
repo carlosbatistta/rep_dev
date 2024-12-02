2fa236cb0a4401a7590e79317f0edf88c1e14cbc
carlosbatistta <carlosn.btt@gmail.com>
2024-12-02 18:27:07 -0300

    add jornada JS

---
+++ dev/JS/jornada/prpjeto_NODE_REACT_NATIVE_TYPESCRIPT/script_funcoes.js
@@ -0,0 +1,38 @@
+var area = document.getElementById('area')
+var areah3 = document.getElementById('areah3')
+
+function entrar() {
+    var nome = prompt("Digite seu nome");
+    if (nome === '' || nome === null) {
+        alert("Ops algo deu errado");
+        area.innerHTML = "Clique no botão para entrar";
+    } else {
+        area.innerHTML = "Bem vindo! " + nome;
+        let botaoMedia = document.createElement("button");
+        botaoMedia.innerText = "Calcular Media";
+        botaoMedia.onclick = media;
+        area.appendChild(botaoMedia);
+        let botaoSair = document.createElement("button");
+        botaoSair.innerText = "Sair";
+        botaoSair.onclick = sair;
+        area.appendChild(botaoSair);
+    }
+}
+
+function sair() {
+    alert("Saiu!")
+    area.innerHTML("Você saiu!")
+}
+
+function media(nota1, nota2) {
+    var lista_notas = [];
+    var lista_notas[1] = prompt("Informe a 1ª nota: ");
+    var nota2 = prompt("Informe a 2ª nota: ");
+    var media = ((nota1 * 1) + (nota2 * 1)) / 2;
+    if (media >= 7) {
+        areah3.innerHTML = "Aluno aprovado com a média: " + media;
+    } else {
+        areah3.innerHTML = "Aluno reprovado com a média: " + media;
+    }
+}
+
