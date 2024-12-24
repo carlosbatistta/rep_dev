2bf9d18e12997c02b3a663b26fdeab2dadba9a42
carlosbatistta <carlosn.btt@gmail.com>
2024-12-24 11:58:02 -0300

    add modulo07

--- dev/JS/jornada/prpjeto_NODE_REACT_NATIVE_TYPESCRIPT/modulo_07/backend/src/routes.ts
+++ dev/JS/jornada/prpjeto_NODE_REACT_NATIVE_TYPESCRIPT/modulo_07/backend/src/routes.ts
@@ -36,31 +36,31 @@ router.post('/users', new CreateUserController().handle)
 
 router.post('/session', new AuthUserController().handle)
 
-router.get('/me', isAuthenticated,  new DetailuserController().handle )
+router.get('/me', isAuthenticated, new DetailuserController().handle)
 
 //-- ROTAS CATEGORY
-router.post('/category', isAuthenticated, new CreateCategoryController().handle )
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
-router.get('/category', isAuthenticated, new ListCategoryController().handle )
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 //-- ROTAS PRODUCT
-router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle )
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
-router.get('/category/product', isAuthenticated, new ListByCategoryController().handle )
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
 
 //-- ROTAS ORDER
-router.post('/order', isAuthenticated, new CreateOrderController().handle )
-router.delete('/order', isAuthenticated, new RemoveOrderController().handle )
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
-router.post('/order/add', isAuthenticated, new AddItemController().handle )
-router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle )
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
 
-router.put('/order/send', isAuthenticated, new SendOrderController().handle )
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 
-router.get('/orders', isAuthenticated, new ListOrdersController().handle )
-router.get('/order/detail', isAuthenticated, new DetailOrderController().handle )
+router.get('/orders', isAuthenticated, new ListOrdersController().handle)
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
 
-router.put('/order/finish', isAuthenticated, new FinishOrderController().handle )
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle)
 
 
 
